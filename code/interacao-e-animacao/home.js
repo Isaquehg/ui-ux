f8a973113bc47026728fa5746a5785a632db3381
Isaquehg <isaqueh.goncalves@gmail.com>
2024-05-31 23:14:48 -0300

    dark mode

--- code/interacao-e-animacao/home.js
+++ code/interacao-e-animacao/home.js
@@ -5,4 +5,17 @@ document.getElementById('toggle-date-picker').addEventListener('click', function
 });
 
 // Light and Dark mode
+document.addEventListener('DOMContentLoaded', function () {
+    const icon = document.getElementById('dark-mode-icon');
+
+    icon.addEventListener('click', function () {
+        icon.classList.toggle('dark-mode');
+        // Toggle the icon between light and dark mode
+        if (icon.classList.contains('dark-mode')) {
+            icon.textContent = 'brightness_7'; // Change icon to represent light mode
+        } else {
+            icon.textContent = 'brightness_4'; // Change icon to represent dark mode
+        }
+    });
+});
 
