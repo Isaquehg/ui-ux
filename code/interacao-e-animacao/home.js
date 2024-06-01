8c736b9604da7dda5040d0790c88198aba0547f4
Isaquehg <isaqueh.goncalves@gmail.com>
2024-05-31 23:04:42 -0300

    interacao e animacao

---
+++ code/interacao-e-animacao/home.js
@@ -0,0 +1,8 @@
+// Hide and show data picker filter
+document.getElementById('toggle-date-picker').addEventListener('click', function() {
+    var datePickerContainer = document.getElementById('date-picker-container');
+    datePickerContainer.classList.toggle('show');
+});
+
+// Light and Dark mode
+
